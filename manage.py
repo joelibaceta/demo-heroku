b575f5c5f695263084a9f0f323a53b73273b9805
joel ibaceta <mail@joelibaceta.com>
2021-11-23 20:51:54 -0600

    update initialization

--- manage.py
+++ manage.py
@@ -3,6 +3,11 @@
 import os
 import sys
 
+def manual():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld.settings')
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'runserver'])
+
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +20,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+ 
     execute_from_command_line(sys.argv)
 
 
