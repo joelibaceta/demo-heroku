235c6fd9d1ff189ec4de7f82c4953e3860daebac
joel ibaceta <mail@joelibaceta.com>
2021-11-23 18:16:29 -0600

    append urls

--- helloworld/helloworld/urls.py
+++ helloworld/helloworld/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import HelloView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HelloView.as_view())
 ]
